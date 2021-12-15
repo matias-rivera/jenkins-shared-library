868fe8c841854ed282650fc916acd76e5429d7f8
matias-rivera <juanmatiasrivera@outlook.com>
2021-12-15 11:49:01 -0300

    add shared library

---
+++ vars/runLinuxScript.groovy
@@ -0,0 +1,8 @@
+def call(Map config = [:]) {
+  def scriptcontents = libraryResource "scripts/${config.name}"
+  writeFile file: "${config.name}", text: scriptcontents
+  sh """
+    chmod a+x ./${config.name}
+    ./${config.name}
+  """
+}
